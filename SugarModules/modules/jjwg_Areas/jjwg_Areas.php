3625b184f0ab7e6ff6c5f7bea263b840a3664fa7
Jeff Walters <jjwdesign@gmail.com>
2013-07-16 22:41:22 -0400

    Areas' Point in Polygon Check

--- SugarModules/modules/jjwg_Areas/jjwg_Areas.php
+++ SugarModules/modules/jjwg_Areas/jjwg_Areas.php
@@ -13,6 +13,11 @@ class jjwg_Areas extends jjwg_Areas_sugar {
      * @var settings array
      */
     var $settings = array();
+    /**
+     * Point in Area/Polygon check on vertices?
+     * @var boolean
+     */
+    var $point_on_vertex = true;
 
     function jjwg_Areas() {
         
@@ -145,5 +150,109 @@ class jjwg_Areas extends jjwg_Areas_sugar {
     function is_valid_lat($lat) {
         return (is_numeric($lat) && $lat >= -90 && $lat <= 90);
     }
+
+    /**
+     * Determine if Marker Object is in Area (Polygon)
+     * @param object $marker
+     */
+    function is_marker_in_area($marker) {
+        
+        $loc = array();
+        if (is_object($marker)) {
+            $loc['lat'] = $marker->jjwg_maps_lat;
+            $loc['lng'] = $marker->jjwg_maps_lng;
+        } elseif (is_array($marker)) {
+            $loc['lat'] = $marker['lat'];
+            $loc['lng'] = $marker['lng'];
+        }
+        
+        return $this->is_point_in_area($loc['lng'], $loc['lat']);
+    }
+
+    /**
+     * Determine if lng/lat point is in Area Polygon
+     * 
+     * @param float $lng
+     * @param float $lat
+     */
+    function is_point_in_area($lng, $lat) {
+        
+        // lng,lat,elv
+        $point = $lng.','.$lat.',0.0';
+        
+        return $this->point_in_polygon($point);
+    }
+    
+    /**
+     * Determine if Point lies within Polygon
+     * 
+     * Polygon Algorithm: http://assemblysys.com/php-point-in-polygon-algorithm/
+     * Modified by JJWDesign 07/16/2013
+     *
+     * @param string $point
+     * @param boolean $point_on_vertex
+     * @return boolean
+     */
+    function point_in_polygon($point, $point_on_vertex = true) {
+        
+        $this->point_on_vertex = $point_on_vertex;
+        $polygon = preg_split('/[\s]+/', $this->coordinates);
+ 
+        // Transform string coordinates into arrays with x and y values
+        $point = $this->point_string_to_coordinates($point);
+        $vertices = array();
+        foreach ($polygon as $vertex) {
+            $vertices[] = $this->point_string_to_coordinates($vertex);
+        }
+        
+        // Check if the point sits exactly on a vertex
+        if ($this->point_on_vertex == true and $this->point_on_vertex($point, $vertices) == true) {
+            return true;
+        }
+ 
+        // Check if the point is inside the polygon or on the boundary
+        $intersections = 0;
+        $vertices_count = count($vertices);
+ 
+        for ($i=1; $i < $vertices_count; $i++) {
+            $vertex1 = $vertices[$i-1];
+            $vertex2 = $vertices[$i];
+            if ($vertex1['y'] == $vertex2['y'] and $vertex1['y'] == $point['y'] and $point['x'] > min($vertex1['x'], $vertex2['x']) and $point['x'] < max($vertex1['x'], $vertex2['x'])) { // Check if point is on an horizontal polygon boundary
+                return true;
+            }
+            if ($point['y'] > min($vertex1['y'], $vertex2['y']) and $point['y'] <= max($vertex1['y'], $vertex2['y']) and $point['x'] <= max($vertex1['x'], $vertex2['x']) and $vertex1['y'] != $vertex2['y']) {
+                $xinters = ($point['y'] - $vertex1['y']) * ($vertex2['x'] - $vertex1['x']) / ($vertex2['y'] - $vertex1['y']) + $vertex1['x'];
+                if ($xinters == $point['x']) { // Check if point is on the polygon boundary (other than horizontal)
+                    return true;
+                }
+                if ($vertex1['x'] == $vertex2['x'] || $point['x'] <= $xinters) {
+                    $intersections++;
+                }
+            }
+        }
+        // If the number of edges we passed through is odd, then it's in the polygon.
+        if ($intersections % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+ 
+    function point_on_vertex($point, $vertices) {
+        
+        foreach($vertices as $vertex) {
+            if ($point == $vertex) {
+                return true;
+            }
+        }
+    }
+
+    function point_string_to_coordinates($pointString) {
+        
+        // Coordinate Results (lng,lat,elv)
+        $coordinates = preg_split('/[,]+/', $pointString);
+        return array("x" => $coordinates[0], "y" => $coordinates[1]);
+    }
+    
 
 }
