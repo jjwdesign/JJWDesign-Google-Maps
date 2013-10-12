3717bb2bbbd5f33395fae8bdf8411ba98796bed7
Jeff Walters <jjwdesign@gmail.com>
2013-10-11 23:13:13 -0400

    Updating Notes and Manifest

--- manifest.php
+++ manifest.php
@@ -13,18 +13,18 @@ $manifest = array (
         ),
     'key'=>'jjwg',
     'author' => 'Jeff Walters',
-    'description' => 'JJWDesign Google Maps v2.1.2 for SugarCRM v6.2+',
+    'description' => 'JJWDesign Google Maps v2.1.3 for SugarCRM v6.2+',
     'icon' => 'images/default/images/icon_jjwg_Maps.gif',
     'is_uninstallable' => true,
     'name' => 'JJWDesign_Google_Maps',
-    'published_date' => '2013-10-07 11:11:11',
+    'published_date' => '2013-10-11 12:12:12',
     'type' => 'module',
-    'version' => '2.1.2-6.2+',
+    'version' => '2.1.3-6.2+',
     'remove_tables' => 'prompt'
 );
 
 $installdefs = array (
-  'id' => 'Google_Maps_20131007',
+  'id' => 'Google_Maps_20131011',
 
   'beans' => 
   array (
